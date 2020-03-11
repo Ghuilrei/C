d5900ccc2d0e6cdce969d7af7ac23d47d3e9d30f
FerdinSun <sunyh512@163.com>
2020-03-11 17:06:19 +0800

    Initial Commit

---
+++ C_HomeWork/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C_HomeWork
+//
+//  Created by 孙宇航 on 2020/3/11.
+//  Copyright © 2020 孙宇航. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
